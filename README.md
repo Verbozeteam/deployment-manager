6bd09ee6d4a8e26f3fcb85a071077e530157c365
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2017-11-08 17:09:56 +0300

    Pimped it more

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # deployment-manager
+
+## Pre-requirements
+To be able to let the Raspberry Pi fetch code from your machine, add the public key of the Pi to your autherized keys:
+```
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9sDAwtfroed7pIApK1491i6hlETx4A4p7h71KMimEQA9xeImtinVO55Jp2nQN5AZaY9v+MV+8u8lAimcy+1R5pJvzrPr7ixx8Z4MCiaIfqNyoTDGicW3BTwrm9H+rp6xnoryxZ1jqV5lLMQBjK6txJT8ahLeJkh2B7zIsqUAI35DvyOOBxYDJbo/5oDluY1GZQ3XCEOWtcoOG0oHFzslJZPXPAuCQ/CCDPorrnGl/mpYm/yNoAcvl6lGKgIqdhYFkpdkQBVbpwmaUUUUX7+TABmJK+Ci7GKd2eoJouqXrgYxeiAJXaom6dsqkhXo0vCbGffMtMr5Yt9NtzzsykaXL pi@raspberrypi' >> ~/.ssh/authorized_keys
+```
+
+## Images
+- unified_2_1.img
+OS: Raspbian for RBP 3.0
+Python: 2
+Comment: Has old code pre-installed
+
+
--- configure-pi.py
+++ configure-pi.py
@@ -20,23 +20,34 @@ parser.add_argument("-ip", "--piip", required=False, type=str, default="192.168.
 parser.add_argument("-u", "--user", required=False, type=str, default="pi", help="Username of the raspberry PI to SSH to")
 parser.add_argument("-p", "--password", required=False, type=str, default="notdefault", help="Password of the raspberry PI to SSH to")
 
-parser.add_argument("-r", "--room", required=True, type=str, help="Name of the room (JSON of the format \{\"en\": \"english name\", \"ar\": \"arabic name\"\})")
-parser.add_argument("-s", "--ssid", required=True, type=str, help="Name of the broadcasted ssid")
+parser.add_argument("-r", "--room", required=False, type=str, help="Name of the room (JSON of the format \{\"en\": \"english name\", \"ar\": \"arabic name\"\})")
+parser.add_argument("-s", "--ssid", required=False, type=str, help="Name of the broadcasted ssid")
 parser.add_argument("-sp", "--ssid_pw", required=False, default="notdefaultatall", type=str, help="Password of the hosted network")
-parser.add_argument("-c", "--channel", required=True, type=int, help="Network channel to use")
-parser.add_argument("-i", "--identity", required=True, type=str, help="Discovery protocol identity (without type, e.g.: Room 4:somedata)")
+parser.add_argument("-c", "--channel", required=False, type=int, help="Network channel to use")
+parser.add_argument("-i", "--identity", required=False, type=str, help="Discovery protocol identity (without type, e.g.: Room 4:somedata)")
 parser.add_argument("-t", "--type", required=False, type=int, default=3, help="Type of the device for the discovery protocol")
+
+parser.add_argument("-cla", "--clone_arduino", required=False, type=str, help="Clone arduino repository")
+parser.add_argument("-clm", "--clone_middleware", required=False, type=str, help="Clone middleware repository")
+parser.add_argument("-cld", "--clone_discovery", required=False, type=str, help="Clone discovery repository")
+parser.add_argument("-stp", "--setup", required=False, action='store_true', help="Perform file setup")
+parser.add_argument("-dla", "--download_arduino", required=False, action='store_true', help="Download Arduino software to Arduino")
 
 cmd_args = parser.parse_args()
 
-ARGUMENTS = {
-    "BLUEPRINT_NAME": cmd_args.room,
-    "SSID": cmd_args.ssid,
-    "CHANNEL": cmd_args.channel,
-    "WPA_PASS": cmd_args.ssid_pw,
-    "IDENTITY_TYPE": cmd_args.type,
-    "IDENTITY_STRING": cmd_args.identity,
-}
+my_username = os.getlogin()
+my_ip = "192.168.10.14"
+
+ARGUMENTS = {}
+if cmd_args.setup:
+    ARGUMENTS = {
+        "BLUEPRINT_NAME": cmd_args.room,
+        "SSID": cmd_args.ssid,
+        "CHANNEL": cmd_args.channel,
+        "WPA_PASS": cmd_args.ssid_pw,
+        "IDENTITY_TYPE": cmd_args.type,
+        "IDENTITY_STRING": cmd_args.identity,
+    }
 
 config_json = json.load(open(os.path.join("configs", CONFIG_NAME, "config.json"), "r"))
 extra_commands = config_json.get("extra_commands", [])
@@ -57,25 +68,40 @@ def find_and_open_file(filename):
             pass
     return None
 
+os.system("echo 'rm -rf /home/pi/initializer/ && mkdir /home/pi/initializer' | sshpass -p {} ssh {}@{}".format(cmd_args.password, cmd_args.user, cmd_args.piip))
+
 os.system("mkdir tmp")
 
 with open("tmp/initializer.sh", "w") as initializer_file:
-    for fname in config_json.keys():
-        target_name = config_json[fname]
-        Fin = find_and_open_file(fname)
-        content = Fin.read()
-        Fin.close()
-        for kw in ARGUMENTS.keys():
-            content = content.replace("{{%s}}" % str(kw), str(ARGUMENTS[kw]))
-        with open(os.path.join("tmp", fname), "w") as F:
-            F.write(content)
+    # Write the cloning of repos to the initializer script
+    if cmd_args.clone_middleware:
+        initializer_file.write("sudo rm -rf /home/pi/middleware && git clone {}@{}:{} /home/pi/middleware\n".format(my_username, my_ip, cmd_args.clone_middleware))
+    if cmd_args.clone_discovery:
+        initializer_file.write("sudo rm -rf /home/pi/discovery && git clone {}@{}:{} /home/pi/discovery\n".format(my_username, my_ip, cmd_args.clone_discovery))
+    if cmd_args.clone_arduino:
+        initializer_file.write("sudo rm -rf /home/pi/arduino && git clone {}@{}:{} /home/pi/arduino\n".format(my_username, my_ip, cmd_args.clone_arduino))
 
-        initializer_file.write("sudo cp /home/pi/initializer/{} {}\n".format(fname, target_name))
-    for c in extra_commands:
-        initializer_file.write("{}\n".format(c))
+    # Write the burning of arduino to the intiializer script
+    if cmd_args.download_arduino:
+        intiializer_file.write("cd /home/pi/arduino/ && ./upload.sh mega2560\n")
+
+    # Write the rest of file copying to the initializer script
+    if cmd_args.setup:
+        for fname in config_json.keys():
+            target_name = config_json[fname]
+            Fin = find_and_open_file(fname)
+            content = Fin.read()
+            Fin.close()
+            for kw in ARGUMENTS.keys():
+                content = content.replace("{{%s}}" % str(kw), str(ARGUMENTS[kw]))
+            with open(os.path.join("tmp", fname), "w") as F:
+                F.write(content)
+
+            initializer_file.write("sudo cp /home/pi/initializer/{} {}\n".format(fname, target_name))
+        for c in extra_commands:
+            initializer_file.write("{}\n".format(c))
     initializer_file.write("sudo rm -rf /home/pi/initializer/\n")
 
-os.system("echo 'rm -rf /home/pi/initializer && mkdir /home/pi/initializer' | sshpass -p {} ssh {}@{}".format(cmd_args.password, cmd_args.user, cmd_args.piip))
 os.system("sshpass -p {} scp -r {} {}@{}:{}".format(cmd_args.password, "tmp/*", cmd_args.user, cmd_args.piip,  "/home/pi/initializer/"))
 os.system("echo 'chmod +x /home/pi/initializer/initializer.sh && /home/pi/initializer/initializer.sh' | sshpass -p {} ssh {}@{}".format(cmd_args.password, cmd_args.user, cmd_args.piip))
 
