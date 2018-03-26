305040c82787686bf24d1937eedc22a0cbc00851
Mohammed M. Fituri <mfituri94@gmail.com>
2018-03-26 12:51:02 +0300

    refactoring BlueprintEditor

--- interface/frontend/BlueprintEditor.js
+++ interface/frontend/BlueprintEditor.js
@@ -3,188 +3,211 @@ import NiceButton from './NiceButton';
 
 export default class BlueprintEditor extends React.Component {
 
-    state = {
-        viewType: 'structured', /* either structured or dump (dump == textfield) */
-        version: 1,
-        blueprint: {},
+  state = {
+    viewType: 'structured', /* either structured or dump (dump == textfield) */
+    version: 1,
+    blueprint: {},
+    fields: {}
+  }
+
+  globalIds = 0;
+
+  componentWillMount() {
+    this.componentWillReceiveProps(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps()');
+    const { content } = nextProps;
+
+    this.v1ToEditorFormat(content);
+  }
+
+  /* create exportable array object in internal format */
+  createArray(array, addables) {
+    return {
+      _type: 'array',
+      id: this.globalIds++,
+      collapsed: false,
+      array,
+      addables,
+    };
+  }
+
+  /* create exportable Javascript object object in internal format */
+  createObject(object) {
+    return {
+      _type: 'object',
+      id: this.globalIds++,
+      collapsed: false,
+      object
+    };
+  }
+
+  /* create exportable editor fields object in internal format */
+  createField(value, type, validator, editable) {
+    const { fields } = this.state;
+
+    if (typeof validator == 'undefined') {
+      validator = () => {};
     }
 
-    addableObjects = [
-        {
-            name: '+ Light Switch',
-            validator: () => {},
-            thing: {
-                category: 'light_switches',
-                name: 'text',
-                switch_port: 'text',
-                on_state: 'number',
-            }
-        }, {
-            name: '+ Dimmer',
-            validator: () => {},
-            thing: {
-                category: 'dimmers',
-                name: 'text',
-                dimmer_port: 'text',
-            }
-        }, {
-            name: '+ Curtain',
-            validator: () => {},
-            thing: {
-                category: 'curtains',
-                name: 'text',
-                up_port: 'text',
-                down_port: 'text',
-                on_state: 'number'
-            }
-        }, {
-            name: '+ Room',
-            validator: () => {},
-        }, {
-            name: '+ Panel',
-            validator: () => {}
-        }
-    ];
-
-    componentWillReceiveProps(nextProps) {
-        console.log('receiving blueprint');
-        const { content } = this.props;
+    if (typeof editable == 'undefined') {
+      editable = true;
     }
 
-    v1ToEditorFormat(blueprint) {
-        /* deep copy blueprint */
-        blueprint = JSON.parse(JSON.stringifgy(blueprint));
+    const id = this.globalIds++;
+
+    /* add initial value in fields */
+    fields[id] = value;
+    this.setState({
+      fields: fields
+    });
+
+    return {
+      _type: 'field',
+      id,
+      type,
+      validator,
+      editable
+    };
+  }
+
+  v1ToEditorFormat(content) {
+    console.log('v1ToEditorFormat()', content);
+
+    const v1 = this.parseJson(content);
+
+    const createNameField = (name) => {
+      return this.createObject({
+        en: this.createField(name.en, 'text'),
+        ar: this.createField(name.ar, 'text')
+      });
     }
 
-    v2ToEditorFormat() {
+    const blueprint = this.createObject({
+      id: this.createField(v1.id, 'number'),
+      rooms: this.createArray(v1.rooms.map(room => this.createObject({
+        name: this.createField(room.name, 'text'),
+        grid: this.createArray(room.grid.map(grid => this.createObject({
+          ratio: this.createField(grid.ratio, 'number'),
+          panels: this.createArray(grid.panels.map(panel => this.createObject({
+            ratio: this.createField(panel.ratio, 'number'),
+            name: createNameField(panel.name),
+            things: this.createArray(panel.things.map(thing => this.createObject({
+              category: this.createField(thing.category, 'text'),
+              name: createNameField(thing.name),
+            })), 'Thing')
+          })), 'Panel')
+        })), 'Grid'),
+        detail: this.createObject({
+          ratio: this.createField(room.detail.ratio, 'number'),
+          side: this.createField(room.detail.side, 'text')
+        }),
+        layout: this.createObject({
+          margin: this.createField(room.layout.margin, 'number')
+        })
+      })), 'Room')
+    });
 
-    }
+    this.setState({
+      blueprint
+    });
+  }
 
-    editorFormatToV1() {
+  // TODO: implement this
+  v2ToEditorFormat(blueprint) {
+  }
 
-    }
+  // TODO: implement this
+  editorFormatToV1() {
+  }
 
-    editorFormatToV2() {
+  // TODO: implement this
+  editorFormatToV2() {
+  }
 
-    }
+  renderViewTypeButtons() {
+    const { viewType, version } = this.state;
 
-    renderViewTypeButtons() {
-        const { viewType, version } = this.state;
-
-        return <div style={styles.horizontal_flex}>
-            <NiceButton onClick={() => this.setState({viewType: 'structured'})}
-                extraStyle={{display: 'flex', flex: 1}}
-                isHighlighted={viewType === 'structured'}>
-                {'structured'}
-                <form>
-                    <input type={'radio'}
-                        checked={version === 1}
-                        onChange={() => this.setState({version: 1})} />v1
-                    <input type={'radio'}
-                        checked={version === 2}
-                        onChange={() => this.setState({version: 2})} />v2
-                </form>
-            </NiceButton>
-            <NiceButton onClick={() => this.setState({viewType: 'dump'})}
-                extraStyle={{display: 'flex', flex: 1}}
-                isHighlighted={viewType === 'dump'}>
-                {'dump'}
-            </NiceButton>
-        </div>
-    }
-
-    renderAddableObjectButton(object, index) {
-        return <NiceButton key={'addable-object-button-' + index}
-            onClick={() => {}}>
-            {object.name}
+    return (
+      <div style={styles.horizontal_flex}>
+        <NiceButton onClick={() => this.setState({viewType: 'structured'})}
+            extraStyle={{display: 'flex', flex: 1}}
+            isHighlighted={viewType === 'structured'}>
+            {'structured'}
+            <form>
+                <input type={'radio'}
+                    checked={version === 1}
+                    onChange={() => this.setState({version: 1})} />v1
+                <input type={'radio'}
+                    checked={version === 2}
+                    onChange={() => this.setState({version: 2})} />v2
+            </form>
         </NiceButton>
+        <NiceButton onClick={() => this.setState({viewType: 'dump'})}
+            extraStyle={{display: 'flex', flex: 1}}
+            isHighlighted={viewType === 'dump'}>
+            {'dump'}
+        </NiceButton>
+      </div>
+    );
+  }
+
+  renderV1Editor() {
+    const { blueprint } = this.state;
+
+    console.log('renderV1Editor()', blueprint);
+  }
+
+  renderDumpEditor(blueprint) {
+    const { content, editable, onChange } = this.props;
+
+    return (
+      <textarea style={{width: '100%'}}
+        disabled={!editable}
+        value={content}
+        onChange={e => onChange({content: e.target.value})}
+        rows={35} />
+    );
+  }
+
+  // TODO: implement this
+  parseJson(json_string) {
+    var index = json_string.indexOf('{{');
+    // FIXME: this will alwasy be stuck at the first occurrence
+    while (index !== -1 && json_string[index-1] !== '\"') {
+      json_string = json_string.replace('{{', '\"{{');
+      json_string = json_string.replace('}}', '}}\"');
+      var index = json_string.indexOf('{{');
+    }
+    return JSON.parse(json_string);
+  }
+
+  render() {
+    const { content } = this.props;
+    const { viewType, version } = this.state;
+
+    var editor = null;
+    if (viewType === 'structured') {
+      if (version === 1) {
+        editor = this.renderV1Editor();
+      }
     }
 
-    parseJson(json_string) {
-        var index = json_string.indexOf('{{');
-        // FIXME: this will always be stuck at the first occurrence
-        while (index !== -1 && json_string[index-1] !== '\"') {
-            json_string = json_string.replace('{{', '\"{{');
-            json_string = json_string.replace('}}', '}}\"');
-            var index = json_string.indexOf('{{');
-        }
-        return JSON.parse(json_string);
+    else {
+      editor = this.renderDumpEditor(content);
     }
 
-    renderStucturedNode(object) {
-        const attributes = Object.keys(object).map((key) => {
-            if (typeof object[key] == 'object') {
-                if (object[key] instanceof Array) {
-                    const array_objects = [];
-                    for (var i = 0; i < object[key].length; i++) {
-                        array_objects.push(this.renderStucturedNode(object[key][i]));
-                    }
-                    return <div key={'structured-node-key-' + key}
-                        style={styles.node_attribute}>
-                        {key}: {array_objects}
-                    </div>;
-                }
-                else {
-                    return this.renderStucturedNode(object[key]);
-                }
-            } else {
-                return <div key={'structued-node-key-' + key}
-                    style={styles.node_attribute}>
-                    {key}: {object[key]}
-                </div>;
-            }
-        });
-
-        return <div style={styles.node}>
-            {attributes}
-        </div>;
-    }
-
-    renderStructuredV1Editor(object) {
-      return this.renderStucturedNode(object);
-    }
-
-    render() {
-        const { content, editable, onChange } = this.props;
-        const { viewType, version } = this.state;
-
-        /* render addable objects buttons */
-        const addable_objects = [];
-        for (var i = 0; i < this.addableObjects.length; i++) {
-            addable_objects.push(
-                this.renderAddableObjectButton(this.addableObjects[i], i));
-        }
-
-        var editor = null;
-        if (viewType === 'structured') {
-            if (version === 1) {
-                editor = this.renderStructuredV1Editor(this.parseJson(content));
-            }
-        }
-
-        else if (viewType === 'dump') {
-            editor = <textarea style={{width: '100%'}}
-                disabled={!editable}
-                value={content}
-                onChange={() => onChange({content: e.target.value})}
-                rows={35} />;
-        }
-
-        else {
-            throw 'مستحيل'
-        }
-
-        return (
-            <React.Fragment>
-                <h3>Blueprint Editor</h3>
-                {this.renderViewTypeButtons()}
-                {editor}
-                <div style={styles.horizontal_flex}>
-                    {addable_objects}
-                </div>
-            </React.Fragment>
-        );
-    }
+    return (
+      <React.Fragment>
+        <h3>Blueprint Editor</h3>
+        {this.renderViewTypeButtons()}
+        <hr />
+        {editor}
+      </React.Fragment>
+    );
+  }
 }
 
 const styles = {
