a420e6e73a0604b79d44ae738d575fdc94181a7e
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-03-26 11:48:15 +0300

    Added pulling each branch

--- interface/ui/tasks.py
+++ interface/ui/tasks.py
@@ -11,7 +11,7 @@ def fetch_repositories():
     for R in repos:
         if len(R.remote_path) > 0 and len(R.local_cache) > 0:
             # clone locally
-            fetch_all_branchs = """for branch in `git branch -r | grep -v HEAD | grep -v master`; do sudo git checkout "${branch#origin/}"; done; git checkout master"""
+            fetch_all_branchs = """for branch in `git branch -r | grep -v HEAD | grep -v master`; do sudo git checkout "${branch#origin/}" && git pull; done; git checkout master"""
             if os.path.isdir(R.local_cache):
                 os.system("eval \"$(ssh-agent -s)\" && ssh-add /home/pi/.ssh/id_rsa && cd {} && {} && sudo killall ssh-agent".format(R.local_cache, fetch_all_branchs))
             else:
